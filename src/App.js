f201a3c12f3dfa85633e51338363959bb11e25e5
jimmyDev <jimmybuidev@gmail.com>
2023-11-03 04:57:27 -0700

    intro page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div id='intro'>
+      <div className='container'>
+        <h1 className='title'>
+          Hello, I am <span class='name-color'>Jimmy Bui</span> <br/>
+          Full-Stack Developer 
+        </h1>
+        <p className='reveal'>
+          <span className='cta cta-button'><a>Know more</a></span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
+    
+    
   );
 }
 
