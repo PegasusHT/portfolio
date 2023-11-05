b4a3fdf3b9d581ae084c544eb2d0758b303ce803
jimmyDev <jimmybuidev@gmail.com>
2023-11-05 12:30:59 -0700

    intro

--- src/components/sections/about/about.js
+++ src/components/sections/about/about.js
@@ -1,21 +1,55 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
+import profile_pic from '../../../images/profile_horizontal.jpg'
 
+const About = () => {
+    return (
+        <section id='about'>
+            <Container className='about-container'>
+                <Row >
+                    <Title title = 'About Me'/>
+                </Row>
+                <Row className='row-bot'>
+                    <Col className='profile-col'>
+                        <div className='profile-div'>
+                            <ProfilePicture />
+                        </div>
+                    </Col>
+                    <Col md={6} sm={12}>
+                        <div className='div-content'>
+                            <p className='content'> 
+                                <p> I am recent graduate full-stack developer who
+                                is passionate  
+                                 about building awesome website’s and application’s! I have 
+                                 currently learned and used HTML, CSS, SASS, Javascript, 
+                               React. On personal level, I am self-driven & hard working, 
+                                 I am able to adapt very quick and overall I am very curious.
+                                Also, I am really friendly who can fit in any team. </p>
+                            <br/>
+                            <p> My current skills  </p>
+                            <br/>
+                            <p> HTML, CSS, SASS, Javascript, React, NodeJs, SQL,
+                                Jest, Cucumber, Docker, GIT</p>
+                            </p>
+                        </div>
+                        
+                    </Col>
+                </Row>
+            </Container> 
+        </section>
+    )
+}
+ 
 const Title = ({ title }) => (
     <Fade bottom duration={1000} delay={300} distance="0px">
       <h2 className="section-title">{title}</h2>
     </Fade>
 );
 
-const About = () => {
-    return (
-        <section id='about'>
-            <Container>
-                <Title title = 'About Me'/>
-
-            </Container>
-        </section>
-    )
-}
+const ProfilePicture = () => (
+    <Fade left duration={1000} delay={300} distance='10px'>
+        <img src={profile_pic} className='profile_pic' alt='profile picture' />
+    </Fade> 
+)
 
 export default About;
--- src/components/sections/intro/intro.js
+++ src/components/sections/intro/intro.js
@@ -1,22 +1,43 @@
 import { Link } from 'react-scroll';
+import Fade from 'react-reveal/Fade';
+import React, { useState, useEffect } from 'react';
 
 const Intro = () => {
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
     return (
-        <div id='intro'>
-          <div className='container'>
-            <h1 className='title'>
-              Hello, I am <span class='name-color'>Jimmy Bui</span> <br/>
-              Full-Stack Developer 
-            </h1>
-            <p className='reveal'>
-              <span className='cta cta--button'>
-                <Link to="about" smooth duration={1000}>
-                    Know more
-                </Link>
-              </span>
-            </p>
-          </div>
+      <section id='intro' className='jumbotron'>
+        <div className='welcome'>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+              <h1 className='title'>
+                Hello, I am <span className='name-color'>Jimmy Bui</span> <br/>
+                Full-Stack Developer 
+              </h1>
+          </Fade>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <p className='reveal'>
+                <span className='cta cta--button'>
+                  <Link to="about" smooth duration={1000}>
+                      Know more
+                  </Link>
+                </span>
+              </p>
+          </Fade>
         </div>
+      </section>
+          
     )
 }
 
